c9940a8c42b10dc3153b00baa50deb0836f97de8
aires1294 <pedrotja.engenharia@gmail.com>
2025-10-02 12:02:47 -0300

    Refatora app js e css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import Materials from "./components/Materials";
+import HelpResources from "./components/HelpResources";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeroSection />
+      <Materials />
+      <HelpResources />
+    </>
   );
 }
 
